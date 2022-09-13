70f869f7dac14e563a4537f6151d4518c69ad7bd
Vector1331 <tpgus4796@gmail.com>
2022-09-14 00:30:12 +0900

    [MSA1-chap3] v3& v4리펙토링 3계층 애플리케이션 (김세현)

--- MSA1/VECTOR/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
+++ MSA1/VECTOR/social-multiplication/src/main/java/microservices/book/multiplication/service/MultiplicationServiceImpl.java
@@ -1,8 +1,10 @@
 package microservices.book.multiplication.service;
 
 import microservices.book.multiplication.domain.Multiplication;
+import microservices.book.multiplication.domain.MultiplicationResultAttempt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class MultiplicationServiceImpl implements MultiplicationService {
@@ -19,5 +21,29 @@ public class MultiplicationServiceImpl implements MultiplicationService {
         int factorA = randomGeneratorService.generateRandomFactor();
         int factorB = randomGeneratorService.generateRandomFactor();
         return new Multiplication(factorA, factorB);
+    }
+    // 예제 3.12 임시 메소드
+    /*@Override
+    public boolean checkAttempt(final MultiplicationResultAttempt resultAttempt) {
+        return false;
+    }*/
+    @Override
+    public boolean checkAttempt(final MultiplicationResultAttempt attempt) {
+        // 답안을 채점
+        boolean correct = attempt.getResultAttempt() ==
+                attempt.getMultiplication().getFactorA() *
+                        attempt.getMultiplication().getFactorB();
+
+        // 조작된 답안을 방지
+        Assert.isTrue(!attempt.isCorrect(), "채점한 상태로 보낼 수 없습니다!!");
+
+        //복사본 만들고 correct 필드를 상황에 맞게 설정
+        MultiplicationResultAttempt checkedAttempt = new MultiplicationResultAttempt(
+                attempt.getUser(),
+                attempt.getMultiplication(),
+                attempt.getResultAttempt(),
+                correct
+        );
+        return correct;
     }
 }
