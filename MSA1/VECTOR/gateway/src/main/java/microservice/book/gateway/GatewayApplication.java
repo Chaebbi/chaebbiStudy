43705143fd0ccfbf29e6c1b558e65717002f1e4e
Vector1331 <tpgus4796@gmail.com>
2022-09-17 01:11:13 +0900

    [MSA1-chap5] api gateway & zuul 구현

--- MSA1/VECTOR/gateway/src/main/java/microservice/book/gateway/GatewayApplication.java
+++ MSA1/VECTOR/gateway/src/main/java/microservice/book/gateway/GatewayApplication.java
@@ -2,7 +2,9 @@ package microservice.book.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class GatewayApplication {
 
