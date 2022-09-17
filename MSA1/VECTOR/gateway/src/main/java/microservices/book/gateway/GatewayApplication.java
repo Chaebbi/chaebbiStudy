78118ad68bc87303700f501dcbde9bc46f18a10d
Vector1331 <tpgus4796@gmail.com>
2022-09-17 15:38:59 +0900

    [MSA1-chap5] Gateway 애플리케이션 에러가 많이 나서 maven 프로젝트로 변경
    하지만 여전히 실행 에러는 동일
    - 이상하다 예제 코드 똑같은거 복사했고 거기선 실행되는데 복사만 해서 오면 안되는 이상한 현상..

--- MSA1/VECTOR/gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
+++ MSA1/VECTOR/gateway/src/main/java/microservices/book/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package microservice.book.gateway;
+package microservices.book.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,8 +8,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 public class GatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GatewayApplication.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(GatewayApplication.class, args);
+  }
 }
